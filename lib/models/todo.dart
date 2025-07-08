62fa994882b679f61c0316fd3106fb6a871075fd
KSTA <>
2025-07-08 10:10:52 +0630

    Add todo provider

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,9 +1,16 @@
+import 'package:flutter/widgets.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:todo_sample/repositories/todo_repository.dart';
+
+part 'todo.g.dart';
+
+@immutable
 class Todo {
   final String id;
   final String title;
   final bool isDone;
 
-  Todo({
+  const Todo({
     required this.id,
     required this.title,
     this.isDone = false,
@@ -19,5 +26,43 @@ class Todo {
       title: title ?? this.title,
       isDone: isDone ?? this.isDone,
     );
+  }
+
+  @override
+  String toString() {
+    return 'Todo(id: $id, title: $title, isDone: $isDone)';
+  }
+}
+
+@riverpod
+class TodoList extends _$TodoList {
+  @override
+  Future<List<Todo>> build() async {
+    final todoRepository = ref.watch(todoRepositoryProvider);
+    return await todoRepository.loadTodos();
+  }
+
+  Future<void> addTodo(String title) async {
+    final todoRepository = ref.watch(todoRepositoryProvider);
+    await todoRepository.addTodo(title);
+    state = AsyncValue.data(await todoRepository.loadTodos());
+  }
+
+  Future<void> toggleTodo(String id) async {
+    final todoRepository = ref.watch(todoRepositoryProvider);
+    await todoRepository.toggleTodo(id);
+    state = AsyncValue.data(await todoRepository.loadTodos());
+  }
+
+  Future<void> updateTodo(Todo todo) async {
+    final todoRepository = ref.watch(todoRepositoryProvider);
+    await todoRepository.updateTodo(todo);
+    state = AsyncValue.data(await todoRepository.loadTodos());
+  }
+
+  Future<void> removeTodo(String id) async {
+    final todoRepository = ref.watch(todoRepositoryProvider);
+    await todoRepository.removeTodo(id);
+    state = AsyncValue.data(await todoRepository.loadTodos());
   }
 }
