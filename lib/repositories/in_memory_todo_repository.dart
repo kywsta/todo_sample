f255da5b9dcec3cd29c1bb0db8e804eaeeba4014
KSTA <>
2025-07-08 10:10:38 +0630

    Add repository

---
+++ lib/repositories/in_memory_todo_repository.dart
@@ -0,0 +1,51 @@
+import 'package:todo_sample/models/todo.dart';
+import 'package:todo_sample/repositories/todo_repository.dart';
+import 'package:uuid/uuid.dart';
+
+class InMemoryTodoRepository extends TodoRepository {
+  final List<Todo> _todos = [];
+
+  InMemoryTodoRepository() {
+    _todos.add(Todo(id: "1", title: "Todo 1"));
+    _todos.add(Todo(id: "2", title: "Todo 2"));
+    _todos.add(Todo(id: "3", title: "Todo 3"));
+  }
+
+  @override
+  Future<List<Todo>> loadTodos() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return _todos;
+  }
+
+  @override
+  Future<void> addTodo(String title) async {
+    final id = Uuid().v4();
+
+    await Future.delayed(const Duration(seconds: 1));
+    _todos.add(Todo(id: id, title: title));
+  }
+
+  @override
+  Future<void> toggleTodo(String id) async {
+    await Future.delayed(const Duration(seconds: 1));
+    final index = _todos.indexWhere((element) => element.id == id);
+    if (index != -1) {
+      _todos[index] = _todos[index].copyWith(isDone: !_todos[index].isDone);
+    }
+  }
+
+  @override
+  Future<void> updateTodo(Todo todo) async {
+    await Future.delayed(const Duration(seconds: 1));
+    final index = _todos.indexWhere((element) => element.id == todo.id);
+    if (index != -1) {
+      _todos[index] = todo;
+    }
+  }
+
+  @override
+  Future<void> removeTodo(String id) async {
+    await Future.delayed(const Duration(seconds: 1));
+    _todos.removeWhere((element) => element.id == id);
+  }
+}
---
+++ lib/repositories/todo_repository.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:todo_sample/app_env.dart';
+import 'package:todo_sample/models/todo.dart';
+import 'package:todo_sample/repositories/in_memory_todo_repository.dart';
+
+part 'todo_repository.g.dart';
+
+abstract class TodoRepository {
+  Future<List<Todo>> loadTodos();
+  Future<void> addTodo(String title);
+  Future<void> toggleTodo(String id);
+  Future<void> updateTodo(Todo todo);
+  Future<void> removeTodo(String id);
+}
+
+@riverpod
+TodoRepository todoRepository(Ref ref) {
+  final appEnv = ref.watch(appEnvProvider);
+  switch (appEnv) {
+    case AppEnv.demo:
+      return InMemoryTodoRepository();
+    case AppEnv.dev:
+      return InMemoryTodoRepository();
+  }
+}
